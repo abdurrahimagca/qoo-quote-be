516bb49e9c6452a7620d25f1eb54c73a0e6eb7ee
abdurrahimagca <agcaabdurrahim@outlook.com>
2025-03-14 14:42:03 +0300

    chore(docker): enhance Docker Compose configuration for qoo-quote service with Traefik labels and update network settings

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: ['*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', "qq-api-key"],
   });
+
   await app.listen(3005, '0.0.0.0');
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
